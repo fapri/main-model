322e12937d9496e9d7a772b835f08d86672c901f
Daniel Jaegers <daj6gd@mail.missouri.edu>
2019-02-19 17:42:10 -0600

    Added crop year data and imported it

--- Corn/Main.R
+++ Corn/Main.R
@@ -2,11 +2,13 @@
 
 library(readxl)
 
+Corn_CropYears <- read_excel("Data/Corn_CropYears.xlsx", sheet = "Sheet1", col_types = c("text", "date", "date"))
 Corn_FuturesMarket <- read_excel("Data/Corn_FuturesMarket.xlsx", sheet = "Sheet1", col_types = c("date", "numeric", "numeric"))
 Corn_Basis <- read_excel("Data/Corn_Basis.xlsx", sheet = "Sheet1")
 Corn_Baseline <- read_excel("Data/Corn_Baseline.xlsx", sheet = "Sheet1", col_types = c("date", 
                                                             "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", 
                                                             "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+lockBinding("Corn_CropYears", globalenv())
 lockBinding("Corn_FuturesMarket", globalenv())
 lockBinding("Corn_Basis", globalenv())
 lockBinding("Corn_Baseline", globalenv())
--- Soybean/Main.R
+++ Soybean/Main.R
@@ -2,11 +2,13 @@
 
 library(readxl)
 
+Soybean_CropYears <- read_excel("Data/Soybean_CropYears.xlsx", sheet = "Sheet1", col_types = c("text", "date", "date"))
 Soybean_FuturesMarket <- read_excel("Data/Soybean_FuturesMarket.xlsx", sheet = "Sheet1", col_types = c("date", "numeric", "numeric"))
 Soybean_Basis <- read_excel("Data/Soybean_Basis.xlsx", sheet = "Sheet1")
 Soybean_Baseline <- read_excel("Data/Soybean_Baseline.xlsx", sheet = "Sheet1", col_types = c("date", 
                                                                                        "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", 
                                                                                        "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+lockBinding("Soybean_CropYears", globalenv())
 lockBinding("Soybean_FuturesMarket", globalenv())
 lockBinding("Soybean_Basis", globalenv())
 lockBinding("Soybean_Baseline", globalenv())
