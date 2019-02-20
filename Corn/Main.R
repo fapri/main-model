299ada65779c420ee991b9540b575e6f86fc7133
Daniel Jaegers <daj6gd@mail.missouri.edu>
2019-02-19 18:04:12 -0600

    Created the main model list and started a function that creates the crop year objects

--- Corn/Main.R
+++ Corn/Main.R
@@ -12,3 +12,17 @@ lockBinding("Corn_CropYears", globalenv())
 lockBinding("Corn_FuturesMarket", globalenv())
 lockBinding("Corn_Basis", globalenv())
 lockBinding("Corn_Baseline", globalenv())
+
+Corn_CropYearObjects = list()
+for(i in 1:nrow(Corn_CropYears)) {
+  Corn_CropYearObjects[i] = createCropYear(Corn_CropYears[i,1], Corn_CropYears[i,2], Corn_CropYears[i,3])
+}
+
+createCropYear <- function(cropYear, startDate, stopDate) {
+  cropYearObj = list(cropYear = cropYear)
+  
+  # TODO Match the baseline, basis, and historical futures market prices which lie between startDate and stopDate
+  # set any other constant variables such as number of sales, percentage sold, etc...
+  
+  return(cropYearObj)
+}
