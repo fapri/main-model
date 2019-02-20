67bc2104e4e2d2c6f249f15848f56b618990db6b
Daniel Jaegers <daj6gd@mail.missouri.edu>
2019-02-19 18:54:51 -0600

    List of crop years now works correctly! Also, changed the order of some code.

--- Corn/Main.R
+++ Corn/Main.R
@@ -13,16 +13,25 @@ lockBinding("Corn_FuturesMarket", globalenv())
 lockBinding("Corn_Basis", globalenv())
 lockBinding("Corn_Baseline", globalenv())
 
-Corn_CropYearObjects = list()
-for(i in 1:nrow(Corn_CropYears)) {
-  Corn_CropYearObjects[i] = createCropYear(Corn_CropYears[i,1], Corn_CropYears[i,2], Corn_CropYears[i,3])
-}
-
+# Creates a crop year based on the input parameters
 createCropYear <- function(cropYear, startDate, stopDate) {
-  cropYearObj = list(cropYear = cropYear)
-  
   # TODO Match the baseline, basis, and historical futures market prices which lie between startDate and stopDate
-  # set any other constant variables such as number of sales, percentage sold, etc...
+  # TODO set any other constant variables such as number of sales, percentage sold, etc
+  
+  model <- data.frame(
+    emp_id = c (1:5), 
+    emp_name = c("Rick","Dan","Michelle","Ryan","Gary"),
+    salary = c(623.3,515.2,611.0,729.0,843.25), 
+    start_date = as.Date(c("2012-01-01", "2013-09-23", "2014-11-15", "2014-05-11", "2015-03-27")),
+    stringsAsFactors = FALSE
+  )
+  
+  cropYearObj = list("Crop Year" = cropYear, "Model" = model)
   
   return(cropYearObj)
+}
+
+Corn_CropYearObjects = list()
+for(i in 1:nrow(Corn_CropYears)) {
+  Corn_CropYearObjects[[i]] = createCropYear(Corn_CropYears[i,1], Corn_CropYears[i,2], Corn_CropYears[i,3])
 }
