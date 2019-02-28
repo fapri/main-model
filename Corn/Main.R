3962ed36ba445d8874013acc57f41b2efe7cc915
Daniel Jaegers <daj6gd@mail.missouri.edu>
2019-02-28 14:39:55 -0600

    progress on basis adjustments

--- Corn/Main.R
+++ Corn/Main.R
@@ -28,53 +28,52 @@ createCropYear <- function(cropYear, startDate, stopDate) {
   marketingYearPost <- setNames(marketingYearPost, c("Date","Price"))
   marketingYear <- rbind(marketingYearPre, marketingYearPost)
   
-  marketingYear[,c("Baseline", "60th", "70th", "80th", "90th", "95th")] <- NA
+  marketingYear[,c("Baseline", "60th", "70th", "80th", "90th", "95th", "Basis")] <- NA
   
   interval1 = interval(mdy(startDate) - months(5), mdy(marchUpdate1) - days(1))
   interval2 = interval(mdy(marchUpdate1), mdy(harvest) - days(1))
   interval3 = interval(mdy(harvest), mdy(marchUpdate2) - days(1))
   interval4 = interval(mdy(marchUpdate2), mdy(stopDate))
-  
-  # TODO Factor in the Basis changes! 
+
   for(row in 1:nrow(marketingYear)) {
     if(mdy(marketingYear$Date[row]) %within% interval1) {
-      marketingYear[row, "Baseline"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 8]
-      marketingYear[row, "60th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 9]
-      marketingYear[row, "70th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 10]
-      marketingYear[row, "80th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 11]
-      marketingYear[row, "90th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 12]
-      marketingYear[row, "95th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 13]
-      
-      # marketingYear[row, "Basis"] = Corn_Basis[which((Corn_Basis$CropYear) %within% intervalBasis), 2]
+      basis = marketingYear[row, "Basis"] = Corn_Basis[which(Corn_Basis$CropYearStart == year(interval1$start)), 3]
+      marketingYear[row, "Baseline"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 8] - basis
+      marketingYear[row, "60th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 9] - basis
+      marketingYear[row, "70th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 10] - basis
+      marketingYear[row, "80th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 11] - basis
+      marketingYear[row, "90th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 12] - basis
+      marketingYear[row, "95th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval1), 13] - basis
     }
     else if(mdy(marketingYear$Date[row]) %within% interval2) {
-      marketingYear[row, "Baseline"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 8]
-      marketingYear[row, "60th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 9]
-      marketingYear[row, "70th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 10]
-      marketingYear[row, "80th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 11]
-      marketingYear[row, "90th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 12]
-      marketingYear[row, "95th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 13]
+      basis = marketingYear[row, "Basis"] = Corn_Basis[which(Corn_Basis$CropYearEnd == year(interval2$start)), 3]
+      marketingYear[row, "Baseline"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 8] - basis
+      marketingYear[row, "60th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 9] - basis
+      marketingYear[row, "70th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 10] - basis
+      marketingYear[row, "80th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 11] - basis
+      marketingYear[row, "90th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 12] - basis
+      marketingYear[row, "95th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval2), 13] - basis
     }
     else if(mdy(marketingYear$Date[row]) %within% interval3) {
-      marketingYear[row, "Baseline"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 2]
-      marketingYear[row, "60th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 3]
-      marketingYear[row, "70th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 4]
-      marketingYear[row, "80th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 5]
-      marketingYear[row, "90th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 6]
-      marketingYear[row, "95th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 7]
+      marketingYear[row, "Basis"] = Corn_Basis[which(Corn_Basis$CropYearStart == year(interval3$start)), 3]
+      marketingYear[row, "Baseline"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 2] - basis
+      marketingYear[row, "60th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 3] - basis
+      marketingYear[row, "70th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 4] - basis
+      marketingYear[row, "80th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 5] - basis
+      marketingYear[row, "90th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 6] - basis
+      marketingYear[row, "95th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval3), 7] - basis
     }
     else if(mdy(marketingYear$Date[row]) %within% interval4) {
-      marketingYear[row, "Baseline"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 2]
-      marketingYear[row, "60th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 3]
-      marketingYear[row, "70th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 4]
-      marketingYear[row, "80th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 5]
-      marketingYear[row, "90th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 6]
-      marketingYear[row, "95th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 7]
+      marketingYear[row, "Basis"] = Corn_Basis[which(Corn_Basis$CropYearEnd == year(interval4$start)), 3]
+      marketingYear[row, "Baseline"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 2] - basis
+      marketingYear[row, "60th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 3] - basis
+      marketingYear[row, "70th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 4] - basis
+      marketingYear[row, "80th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 5] - basis
+      marketingYear[row, "90th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 6] - basis
+      marketingYear[row, "95th"] = Corn_Baseline[which(mdy(Corn_Baseline$Date) %within% interval4), 7] - basis
     }
   }
-  
-  # marketingYear[,c("Basis")] <- NA
-  # 
+
   # intervalBasis = interval(mdy(startDate) - months(4), mdy(harvest) - days(1))
   # 
   # for(row in 1:nrow(marketingYear)) {
