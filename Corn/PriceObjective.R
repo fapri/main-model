89c64654c2e4c4a5970f81f885856e8f8d951583
Daniel Jaegers <daj6gd@mail.missouri.edu>
2019-03-05 14:34:43 -0600

    Progress on price objective triggers

--- Corn/PriceObjective.R
+++ Corn/PriceObjective.R
@@ -6,12 +6,38 @@ source('Corn/Main.R')
 
 # Finds all of the triggers for a given crop year
 priceObjectiveTrigger = function(cropYear) {
+  priceObjectiveTriggers = list()
+  triggers = 0
   
+  marketingYear = cropYear[['Marketing Year']]
+  
+  # Checks the first day of the crop year
+  if(marketingYear$Percentile[1] >= 70) {
+    triggers = triggers + 1
+    priceObjectiveTriggers[[triggers]] = c(marketingYear$Date[1], marketingYear$Percentile[1])
+  }
+  
+  # Checks all subsequent days
+  for(row in 2:nrow(marketingYear)) {
+    if(marketingYear$Percentile[row] >= 70) {
+      if(marketingYear$Percentile[row - 1] < marketingYear$Percentile[row]) {
+        triggers = triggers + 1
+        priceObjectiveTriggers[[triggers]] = c(marketingYear$Date[row], marketingYear$Percentile[row])
+      }
+    }
+  }
+
+  cropYear[['PO Triggers']] <- priceObjectiveTriggers
+  
+  return(cropYear)
 }
 
 # Actualizes the sales for a given crop year
 priceObjectiveActualize_v1 = function(cropYear) {
-  
+  # Blah
 }
 
-# Call them functions
+# Gets the price objective triggers for earch crop year
+for(i in 1:length(Corn_CropYearObjects)) {
+  Corn_CropYearObjects[[i]] = priceObjectiveTrigger(Corn_CropYearObjects[[i]])
+}
