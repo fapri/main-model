854b1570606ee4d7407b0801dda1a48f4e58248a
Daniel Jaegers <daj6gd@mail.missouri.edu>
2019-03-05 13:43:52 -0600

    Set the stage for the prive objective script

--- Corn/PriceObjective.R
+++ Corn/PriceObjective.R
@@ -4,4 +4,14 @@
 # Create the corn objects necessary to run the Price Objective strategy
 source('Corn/Main.R')
 
-# Blah
+# Finds all of the triggers for a given crop year
+priceObjectiveTrigger = function(cropYear) {
+  
+}
+
+# Actualizes the sales for a given crop year
+priceObjectiveActualize_v1 = function(cropYear) {
+  
+}
+
+# Call them functions
