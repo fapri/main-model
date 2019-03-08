78110448181f040889897684d3cef2a1e0d932ef
enscully <ensxvd@mail.missouri.edu>
2019-03-08 12:54:49 -0600

    Started work on ATH. It is currently a mess.

--- Corn/PriceObjective.R
+++ Corn/PriceObjective.R
@@ -21,13 +21,120 @@ isPriceObjective = function(previousDayPercentile, currentDayPercentile) {
   return(FALSE)
 }
 
-# Checks...
-isAllTimeHigh = function(cropYear) {
+
+
+
+
+######################################################################################
+### BEGINNING OF THE GIANT MESS    
+######################################################################################
+
+
+
+TDHTriggers = NA
+
+# Checks 5% Drop from Ten Day High
+isTenDayHigh = function(marketingYear, preInterval, postInterval, TDH) {
+  
+
+    # Checks if date is NC
+    if (mdy(marketingYear$Date[i]) %within% preInterval){
+      # Checks if the price is in 95 percentile.
+      # Checks if the price is > 95%TDH for NC
+      if (is.na(TDH$NC[i])){
+        next
+      }
+      else if (marketingYear$Percentile[i] == 95 && marketingYear$Price[i] < TDH$NC[i]){
+        TDHTriggers[i] = marketingYear$Date[i]
+      }
+      else next
+    }
+    
+    # Checks if date is OC
+    if (mdy(marketingYear$Date[i]) %within% postInterval){
+      # Checks if the price is in 95 percentile.
+      # Checks if the price is > 95%TDH for OC
+      if (marketingYear$Percentile[i] == 95 && marketingYear$Price[i] < TDH$NC[i]){
+        TDHTriggers[i] = marketingYear$Date[i]
+      }
+      else next
+    }
+  }
+
+  # TDHTriggers = list()
+  
+  return(TDHTriggers)
+}
+
+
+for(row in 2:nrow(marketingYear)) {
+
+
+for(i in 1){
+  TDHTriggers_out = isTenDayHigh(Corn_CropYearObjects[[i]]$`Marketing Year`, Corn_CropYearObjects[[i]]$`Pre/Post Interval`$intervalPre,
+                                Corn_CropYearObjects[[i]]$`Pre/Post Interval`$intervalPost, Corn_FeaturesObject$`95% of Ten Day High`)
+  
+}
+
+
+
+
+#############
+# marketingYear = Corn_CropYearObjects[[i]]$`Marketing Year`
+# preInterval = Corn_CropYearObjects[[i]]$`Pre/Post Interval`$intervalPre
+# postInterval = Corn_CropYearObjects[[i]]$`Pre/Post Interval`$intervalPost
+# TDH = Corn_FeaturesObject$`95% of Ten Day High`
+
+
+
+
+
+
+# Checks 5% Drop from Ten Day High
+isTenDayHigh = function(marketingYear, preInterval, postInterval, TDH) {
+  
+  
+  # Goes through number of rows in the marketing year
+  for(i in 1:nrow(Corn_CropYearObjects[[1]]$`Marketing Year` = "marketingYear")){
+    # Checks if date is NC
+    if (Corn_CropYearObjects[[1]]$`Marketing Year`$Date[i] = "marketingYear$Date[i]" %within% Corn_CropYearObjects[[1]]$`Pre/Post Interval`$intervalPre = "preInterval"){
+      # Checks if the price is in 95 percentile.
+      # Checks if the price is > 95%TDH for NC
+      if (is.na(Corn_FeaturesObject$`95% of Ten Day High`$NC[i]) = "TDC$NC[i]"){
+        return(FALSE)
+      }
+      else if (Corn_CropYearObjects[[1]]$`Marketing Year`$Percentile[i] = "marketingYear$Percentile[i]" == 95 && Corn_CropYearObjects[[1]]$`Marketing Year`$Price[i] = "marketingYear$Price[i]" < Corn_FeaturesObject$`95% of Ten Day High`$NC[i] = "TDH$NC[i]"){
+        return(TRUE)
+      }
+      else return(FALSE)
+    }
+
+    # Checks if date is OC
+    if (Corn_CropYearObjects[[1]]$`Marketing Year`$Date[i] = "marketingYear$Date[i]" %within% Corn_CropYearObjects[[1]]$`Pre/Post Interval`$intervalPost = "postInterval"){
+      # Checks if the price is in 95 percentile.
+      # Checks if the price is > 95%TDH for OC
+      if (Corn_CropYearObjects[[1]]$`Marketing Year`$Percentile[i] = "marketingYear$Percentile[i]" == 95 && Corn_CropYearObjects[[1]]$`Marketing Year`$Price[i] < Corn_FeaturesObject$`95% of Ten Day High`$OC[i] = "TDH$NC[i]"){
+        return(TRUE)
+      }
+      else return(FALSE)
+    }
+  }
+  
   return(FALSE)
 }
 
+
+
+######################################################################################
+### END OF THE GIANT MESS
+######################################################################################
+
+
+
+
+
 # Checks...
-isTenDayHigh = function(cropYear) {
+isAllTimeHigh = function(cropYear) {
   return(FALSE)
 }
 
@@ -43,14 +150,15 @@ priceObjectiveTrigger = function(cropYear) {
                                                                       "Type" = "Price Objective"))
   }
   
-  for(row in 2:nrow(marketingYear)) {
+
+    for(row in 2:nrow(marketingYear)) {
     if(isPriceObjective(marketingYear$Percentile[row - 1], marketingYear$Percentile[row])) {
       priceObjectiveTriggers = rbind(priceObjectiveTriggers, data.frame("Date" = marketingYear$Date[row], 
                                                                         "Percentile" = marketingYear$Percentile[row],
                                                                         "Type" = "Price Objective"))
     }
+    # else if TDH
     # else if ath...
-    # else if tdh...
   }
 
   cropYear[['PO Triggers']] = priceObjectiveTriggers
@@ -61,4 +169,11 @@ priceObjectiveTrigger = function(cropYear) {
 # Gets the price objective triggers for earch crop year
 for(i in 1:length(Corn_CropYearObjects)) {
   Corn_CropYearObjects[[i]] = priceObjectiveTrigger(Corn_CropYearObjects[[i]])
+  Corn_CropYearObjects[[i]]$`PO Triggers`$Date = mdy(Corn_CropYearObjects[[i]]$`PO Triggers`$Date)
 }
+
+
+
+
+
+
