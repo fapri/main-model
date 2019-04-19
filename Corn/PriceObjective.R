6f42dca3464f095303f9147eae7688a6dd5cfdeb
enscully <ensxvd@mail.missouri.edu>
2019-04-19 10:14:18 -0500

    Implimented TS trigger logic

--- Corn/PriceObjective.R
+++ Corn/PriceObjective.R
@@ -1,15 +1,15 @@
 # Corn
-# Price Objective
+# Trailing Stop
 
-# Create the corn objects necessary to run the Price Objective strategy
+# Create the corn objects necessary to run the Trailing Stop strategy
 source('Corn/Main.R')
 
 # TODO EVENTUALLY WE CAN MOVE THESE TRIGGER FUNCTIONS TO ANOTHER FILE/FOLDER.
 # THEY ARE NOT DEPENDENT TO CORN
 
-# Checks if currentDayPercentile is a price objective trigger
-isPriceObjective = function(previousDayPercentile, currentDayPercentile) {
-  if(currentDayPercentile >= 70 && previousDayPercentile < currentDayPercentile)
+# Checks if currentDayPercentile is a trailing stop trigger
+isTrailingStop = function(previousDayPercentile, currentDayPercentile) {
+  if(previousDayPercentile >= 70 && previousDayPercentile > currentDayPercentile)
     return(T)
   return(F)
 }
@@ -78,41 +78,41 @@ isEndYearTrailingStop = function(date, previousPercentile, currentPercentile, po
 }
 
 # Checks cases where the baseline updates
-isPriceObjectiveSpecial = function(pricePreviousPercentileAbove, currentPrice) {
-  if (currentPrice >= pricePreviousPercentileAbove) {
+isTrailingStopSpecial = function(pricePreviousPercentileBelow, currentPrice) {
+  if (currentPrice <= pricePreviousPercentileBelow) {
     return(T)
   } else 
     return(F)
 }
 
-# Finds all of the price objective triggers for a given crop year
-priceObjectiveTrigger = function(cropYear) {
-  priceObjectiveTriggers = data.frame()
+# Finds all of the trailing stop triggers for a given crop year
+trailingStopTrigger = function(cropYear) {
+  trailingStopTriggers = data.frame()
   
   marketingYear = cropYear[['Marketing Year']]
   
   for(row in 2:nrow(marketingYear)) {
     # Special case for Feb -> March
     if (month(mdy(marketingYear$Date[row])) == 3 && month(mdy(marketingYear$Date[row - 1])) == 2){
-      if(marketingYear$Percentile[row - 1] != 95 && marketingYear$Percentile[row - 1] >= 60) {
+      if(marketingYear$Percentile[row - 1] != 95 && marketingYear$Percentile[row - 1] >= 70) {
         
-        if(marketingYear$Percentile[row - 1] == 60) previousPercentileAbove = "70th"
-        if(marketingYear$Percentile[row - 1] == 70) previousPercentileAbove = "80th"
-        if(marketingYear$Percentile[row - 1] == 80) previousPercentileAbove = "90th"
-        if(marketingYear$Percentile[row - 1] == 90) previousPercentileAbove = "95th"
+        if(marketingYear$Percentile[row - 1] == 70) previousPercentileBelow = "60th"
+        if(marketingYear$Percentile[row - 1] == 80) previousPercentileBelow = "70th"
+        if(marketingYear$Percentile[row - 1] == 90) previousPercentileBelow = "80th"
+        if(marketingYear$Percentile[row - 1] == 95) previousPercentileBelow = "90th"
         
-        pricePreviousPercentileAbove = marketingYear[row, previousPercentileAbove]
+        pricePreviousPercentileBelow = marketingYear[row, previousPercentileBelow]
         
-        if(previousPercentileAbove == "70th") previousPercentileAbove = 70
-        if(previousPercentileAbove == "70th") previousPercentileAbove = 80
-        if(previousPercentileAbove == "70th") previousPercentileAbove = 90
-        if(previousPercentileAbove == "70th") previousPercentileAbove = 95
+        if(previousPercentileBelow == "60th") previousPercentileBelow = 60
+        if(previousPercentileBelow == "70th") previousPercentileBelow = 70
+        if(previousPercentileBelow == "80th") previousPercentileBelow = 80
+        if(previousPercentileBelow == "90th") previousPercentileBelow = 90
         
         # Takes in price for percentile above prevous day, percentile above previous day, current day price
-        if(isPriceObjectiveSpecial(pricePreviousPercentileAbove, marketingYear$Price[row])) {
-          priceObjectiveTriggers = rbind(priceObjectiveTriggers, data.frame("Date" = marketingYear$Date[row], 
-                                                                            "Percentile" = previousPercentileAbove,
-                                                                            "Type" = "Price Objective Special"))
+        if(isTrailingStopSpecial(pricePreviousPercentileBelow, marketingYear$Price[row])) {
+          trailingStopTriggers = rbind(trailingStopTriggers, data.frame("Date" = marketingYear$Date[row], 
+                                                                        "Percentile" = previousPercentileBelow,
+                                                                        "Type" = "Trailing Stop Special"))
         }
       }
     }
@@ -122,43 +122,43 @@ priceObjectiveTrigger = function(cropYear) {
       next
     }
     
-    else if(isPriceObjective(marketingYear$Percentile[row - 1], marketingYear$Percentile[row])) {
-      priceObjectiveTriggers = rbind(priceObjectiveTriggers, data.frame("Date" = marketingYear$Date[row], 
-                                                                        "Percentile" = marketingYear$Percentile[row],
-                                                                        "Type" = "Price Objective"))
+    else if(isTrailingStop(marketingYear$Percentile[row - 1], marketingYear$Percentile[row])) {
+      trailingStopTriggers = rbind(trailingStopTriggers, data.frame("Date" = marketingYear$Date[row], 
+                                                                    "Percentile" = marketingYear$Percentile[row],
+                                                                    "Type" = "Trailing Stop"))
     }
     
     else if (isTenDayHigh(mdy(marketingYear$Date[row]), marketingYear$Price[row], marketingYear$Percentile[row], 
                           cropYear$`Pre/Post Interval`$intervalPre, cropYear$`Pre/Post Interval`$intervalPost, 
                           Corn_FeaturesObject$`95% of Ten Day High`)) {
-      priceObjectiveTriggers = rbind(priceObjectiveTriggers, data.frame("Date" = marketingYear$Date[row], 
-                                                                        "Percentile" = marketingYear$Percentile[row],
-                                                                        "Type" = "Ten Day High"))
+      trailingStopTriggers = rbind(trailingStopTriggers, data.frame("Date" = marketingYear$Date[row], 
+                                                                    "Percentile" = marketingYear$Percentile[row],
+                                                                    "Type" = "Ten Day High"))
     }
     
     else if (isAllTimeHigh(mdy(marketingYear$Date[row]), marketingYear$Price[row], marketingYear$Percentile[row],
                            cropYear$`Pre/Post Interval`$intervalPre, cropYear$`Pre/Post Interval`$intervalPost, 
                            Corn_FeaturesObject$`95% of Ten Day High`, Corn_FeaturesObject$`All Time High`)) {
-      priceObjectiveTriggers = rbind(priceObjectiveTriggers, data.frame("Date" = marketingYear$Date[row], 
-                                                                        "Percentile" = marketingYear$Percentile[row],
-                                                                        "Type" = "All Time High"))
+      trailingStopTriggers = rbind(trailingStopTriggers, data.frame("Date" = marketingYear$Date[row], 
+                                                                    "Percentile" = marketingYear$Percentile[row],
+                                                                    "Type" = "All Time High"))
     }
     
     else if (isEndYearTrailingStop(mdy(marketingYear$Date[row]), marketingYear$Percentile[row - 1], marketingYear$Percentile[row],
                                    cropYear$`Pre/Post Interval`$intervalPost)) {
-      priceObjectiveTriggers = rbind(priceObjectiveTriggers, data.frame("Date" = marketingYear$Date[row], 
-                                                                        "Percentile" = marketingYear$Percentile[row],
-                                                                        "Type" = "End of Year Trailing Stop"))
+      trailingStopTriggers = rbind(trailingStopTriggers, data.frame("Date" = marketingYear$Date[row], 
+                                                                    "Percentile" = marketingYear$Percentile[row],
+                                                                    "Type" = "End of Year Trailing Stop"))
     }
   }
   
-  cropYear[['PO Triggers']] = priceObjectiveTriggers
+  cropYear[['TS Triggers']] = trailingStopTriggers
   
   return(cropYear)
 }
 
-# Gets the price objective triggers for earch crop year
+# Gets the trailing stop triggers for earch crop year
 for(i in 1:length(Corn_CropYearObjects)) {
-  Corn_CropYearObjects[[i]] = priceObjectiveTrigger(Corn_CropYearObjects[[i]])
-  Corn_CropYearObjects[[i]]$`PO Triggers`$Date = mdy(Corn_CropYearObjects[[i]]$`PO Triggers`$Date)
+  Corn_CropYearObjects[[i]] = trailingStopTrigger(Corn_CropYearObjects[[i]])
+  Corn_CropYearObjects[[i]]$`TS Triggers`$Date = mdy(Corn_CropYearObjects[[i]]$`TS Triggers`$Date)
 }
