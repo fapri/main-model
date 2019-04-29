5c497e8ff418a4d4f13e735d63deddfddb1e3c54
enscully <ensxvd@mail.missouri.edu>
2019-04-29 11:03:50 -0500

    Changed graph colors

--- Corn/Graphing.R
+++ Corn/Graphing.R
@@ -3,7 +3,7 @@
 # Graphing
 
 #Set Color Palette
-myColors = c("#ffff00", "#ffff00","#800080", "#800080", "#ed7d31", "#ffff00", "#ffd700", "#FF69B4")
+myColors = c("#ffff00", "#ffff00","#800080", "#800080", "#ed7d31", "#4286f4", "#ff4242", "#FF69B4")
 colorLabels = c("Price Objective", "Price Objective Special","Trailing Stop", "Trailing Stop Special", "Seasonal", "Ten Day High", "All Time High", "End of Year Trailing Stop")
 names(myColors) = colorLabels 
 
