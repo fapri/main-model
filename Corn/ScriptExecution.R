e03a8e282878694e8c610c9370cbece6a9737e34
Daniel Jaegers <daj6gd@mail.missouri.edu>
2019-04-30 14:10:32 -0500

    Added some comments

--- Corn/ScriptExecution.R
+++ Corn/ScriptExecution.R
@@ -1,23 +1,17 @@
+# Corn
 
-
+# Set up the data and Crop Year Objects
 source("Corn/Main.R")
 
+# Run the strategies
 source("Corn/PriceObjective.R")
 source("Corn/PriceObjectiveActualized.R")
 source("Corn/TrailingStop.R")
 source("Corn/TrailingStopActualized.R")
 source("Corn/SeasonalSaleActualized.R")
 
+# Graph the Strategies
 source("Corn/Graphing.R")
 
+# Adjust for storage
 source("Corn/StorageMerge.R")
-
-
-
-
-
-
-
-
-
-
