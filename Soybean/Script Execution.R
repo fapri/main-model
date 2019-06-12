cdc4043c7c6b2981b7710bcf2e70e0fe7c0b64e3
enscully <ensxvd@mail.missouri.edu>
2019-06-12 11:59:35 -0500

    Trying to find a way to compress ggplots

--- Soybean/Script Execution.R
+++ Soybean/Script Execution.R
@@ -27,46 +27,69 @@ source("Soybean/PriceObjectiveActualized.R")
 source("Soybean/TrailingStopActualized.R")
 source("Soybean/Storage.R")
 source("Model/Graphing.R")
-saveRDS(list(Soybean_CropYearObjects, Soybean_CropYears, finalizedPriceObject), file = "appObjectsSoybeanV3base.rds")
+saveRDS(list(Soybean_CropYearObjects, finalizedPriceObject), file = "appObjectsSoybeanV3Base.rds")
 
 # V2
 source("Soybean/PriceObjectiveActualizedV2.R")
 source("Soybean/TrailingStopActualizedV2.R")
 source("Soybean/Storage.R")
 source("Model/Graphing.R")
-saveRDS(list(Soybean_CropYearObjects, Soybean_CropYears, finalizedPriceObject), file = "appObjectsSoybeanV3V2.rds")
+saveRDS(list(Soybean_CropYearObjects, finalizedPriceObject), file = "appObjectsSoybeanV3V2.rds")
 
 # V3
 source("Soybean/PriceObjectiveActualizedV5.R")
 source("Soybean/TrailingStopActualizedV5.R")
 source("Soybean/Storage.R")
 source("Model/Graphing.R")
-saveRDS(list(Soybean_CropYearObjects, Soybean_CropYears, finalizedPriceObject), file = "appObjectsSoybeanV3V3.rds")
+saveRDS(list(Soybean_CropYearObjects, finalizedPriceObject), file = "appObjectsSoybeanV3V3compress.rds")
 
 # V4
 source("Soybean/PriceObjectiveActualizedV6.R")
 source("Soybean/TrailingStopActualizedV6.R")
 source("Soybean/Storage.R")
 source("Model/Graphing.R")
-saveRDS(list(Soybean_CropYearObjects, Soybean_CropYears, finalizedPriceObject), file = "appObjectsSoybeanV3V4.rds")
+saveRDS(list(Soybean_CropYearObjects, finalizedPriceObject), file = "appObjectsSoybeanV3V4.rds")
 
 #V5
 source("Soybean/PriceObjectiveActualizedV7.R")
 source("Soybean/TrailingStopActualizedV7.R")
 source("Soybean/Storage.R")
 source("Model/Graphing.R")
-saveRDS(list(Soybean_CropYearObjects, Soybean_CropYears, finalizedPriceObject), file = "appObjectsSoybeanV3V5.rds")
+saveRDS(list(Soybean_CropYearObjects, finalizedPriceObject), file = "appObjectsSoybeanV3V5.rds")
 
 
 
 
 
 
+for(i in 1:length(Soybean_CropYearObjects)){
+
+  PO = which(names(Soybean_CropYearObjects[[i]]) == "POPlot")
+  POMY = which(names(Soybean_CropYearObjects[[i]]) == "POMYPlot")
+  TS = which(names(Soybean_CropYearObjects[[i]]) == "TSPlot")
+  TSMY = which(names(Soybean_CropYearObjects[[i]]) == "TSMYPlot")
+  SS = which(names(Soybean_CropYearObjects[[i]]) == "SSPlot")
+  SSMY = which(names(Soybean_CropYearObjects[[i]]) == "SSMYPlot")
+  
+  removeThese = c(PO, POMY, TS, TSMY, SS, SSMY)
+  
+  Soybean_CropYearObjects[[i]] = Soybean_CropYearObjects[[i]][-removeThese]
+  
+}
 
 
 
-# Adjust for storage
-source("Soybean/Storage.R")
+Figure1 = Soybean_CropYearObjects[[1]]$POPlot
+
+ggsave("Figure1.tiff", width = 14, height = 8, dpi=100, compression = "lzw")
+
+
+
+
+
+
+which(names(Soybean_CropYearObjects[[i]]) == "TSPlot")# Adjust for storage
+which(names(Soybean_CropYearObjects[[i]]) == "TSPlot")source("Soybean/Storage.R")
 
 # Graph the Strategies
 source("Model/Graphing.R")
