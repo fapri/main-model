daa0934241db0196e0034858d28c671831738511
enscully <ensxvd@mail.missouri.edu>
2019-06-13 15:10:18 -0500

    Do not run when corn or soybean are not selected

--- ModelExecution.R
+++ ModelExecution.R
@@ -17,35 +17,37 @@ getCropType = function(){
 
 type = getCropType()
 
-# Set up the Data and Crop Year Objects
-source("Model/Main.R")
-
-# Create common trigger functions
-source("Model/Triggers.R")
-source("Model/MultiYearTrigger.R")
-
-# Run the strategy triggers
-source("Model/PriceObjective.R")
-
-source("Model/TrailingStop.R")
-source("Model/TrailingStopV3.R")
-
-
-# RUN THE ACTUALIZATION SCRIPTS
-if(type == "soybean"){
-  # prompt to select the actualization version
-} else if(type == "corn"){
-  # prompt to select the actualization version
-} else {
-  # maybe have an option to do both??
+if(type != "quit"){
+  # Set up the Data and Crop Year Objects
+  source("Model/Main.R")
+  
+  # Create common trigger functions
+  source("Model/Triggers.R")
+  source("Model/MultiYearTrigger.R")
+  
+  # Run the strategy triggers
+  source("Model/PriceObjective.R")
+  
+  source("Model/TrailingStop.R")
+  source("Model/TrailingStopV3.R")
+  
+  
+  # RUN THE ACTUALIZATION SCRIPTS
+  if(type == "soybean"){
+    # prompt to select the actualization version
+  } else if(type == "corn"){
+    # prompt to select the actualization version
+  } else {
+    # maybe have an option to do both??
+  }
+  
+  # Adjust for storage
+  # STORAGE WORKS FOR BOTH CORN AND SOYBEANS
+  source("Model/Storage.R")
+  
+  # Graph the Strategies
+  source("Model/Graphing.R")
+  
+  # Code I used to save the objects
+  #saveRDS(list(Soybean_CropYearObjects, Soybean_CropYears, finalizedPriceObject), file = "appObjectsSoybean.rds")
 }
-
-# Adjust for storage
-# STORAGE IS STILL SEPARATE
-source("Model/Storage.R")
-
-# Graph the Strategies
-source("Model/Graphing.R")
-
-# Code I used to save the objects
-#saveRDS(list(Soybean_CropYearObjects, Soybean_CropYears, finalizedPriceObject), file = "appObjectsSoybean.rds")
