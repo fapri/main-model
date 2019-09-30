604c2918095ca37f2bd384bead17c551289679fb
Daniel Jaegers <daj6gd@mail.missouri.edu>
2019-09-30 13:58:52 -0500

    Can now run for a whole folder at once

--- Miscellaneous/Basis Date Matcher Full Folder.R
+++ Miscellaneous/Basis Date Matcher Full Folder.R
@@ -4,18 +4,19 @@ library(lubridate)
 library(dplyr)
 library(svDialogs)
 
+fullSet = data.frame()
 dates = read_excel("Miscellaneous/Dates.xlsx")
 
-# Take in working large basis set if present
-if (dlg_message(message = "Do you have a basis file in progress?", type = "yesno")[["res"]] == "yes") {
-  fullSet = read_csv(file.choose())
-} else {
-  fullSet = data.frame()
-}
-
-# Loop to add multiple locations at once
-while (dlg_message(message = "Add more data?", type = "yesno")[["res"]] == "yes") {
-  basis = read_csv(file.choose(), skip = 1)
+files = list.files("Miscellaneous/Data")
+for (filename in files) {
+  print(paste("FILENAME:", filename))
+  split = unlist(strsplit(filename, " - "))
+  print(paste("LOCATION:", split[1]))
+  print(paste("COMPANY:", split[2]))
+  print("----------------------------------------------------------------------")
+  
+  basis = read_csv(paste("Miscellaneous/Data/", filename, sep = ""), skip = 1)
+  
   # Remove any escape characters
   colnames(basis) = gsub("[\r\n]", "", colnames(basis))
   
@@ -36,14 +37,10 @@ while (dlg_message(message = "Add more data?", type = "yesno")[["res"]] == "yes"
   basis = data.frame(basis)
   dates = data.frame(dates)
   
-  # Add the city and the company to the dataframe
-  city = dlgInput("Enter the City:", Sys.info()["user"])$res
-  company = dlgInput("Enter the Company:", Sys.info()["user"])$res
-  
   # Merge dates and basis
   mergedSet = merge(x = dates, y = basis, by.x = c("Week"), by.y = c("basisWeek"), all.y = TRUE)
-  mergedSet$City = rep(city, nrow(mergedSet))
-  mergedSet$Company = rep(company, nrow(mergedSet))
+  mergedSet$City = rep(split[1], nrow(mergedSet))
+  mergedSet$Company = rep(split[2], nrow(mergedSet))
   
   fullSet = rbind(fullSet, mergedSet)
 }
