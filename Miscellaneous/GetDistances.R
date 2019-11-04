701afa952c8d07a00455e9955025b82d801bdf6b
Daniel Jaegers <daj6gd@mail.missouri.edu>
2019-11-04 14:30:05 -0600

    Distances from Montgomery County

---
+++ Miscellaneous/GetDistances.R
@@ -0,0 +1,46 @@
+library(readxl)
+library(gmapsdistance)
+
+countyCenters = read_excel("Miscellaneous/County Centers.xlsx")
+
+montgomeryCounty = countyCenters[which(countyCenters$County == "Montgomery"),]
+montgomeryCounty_origin = paste(montgomeryCounty$Latitude, montgomeryCounty$Longitude, sep = "+")
+
+countyCenters = countyCenters[which(countyCenters$County != "Montgomery"),]
+
+results = list()
+errors = list()
+set.api.key("AIzaSyB537MHNRG_GIXDkln-n4oTF7aMxPaiTWM")
+for (row in 1:nrow(countyCenters)) {
+  county = countyCenters[row,]
+  county_name = county$County
+  county_dest = paste(county$Latitude, county$Longitude, sep = "+")
+  
+  result = gmapsdistance(origin = montgomeryCounty_origin,
+                          destination = county_dest,
+                          mode = "driving")
+  
+  if (result$Status == "OK") {
+    results[[county_name]] = result$Distance
+  } else {
+    errors[[county_name]] = result$Status
+  }
+}
+
+
+Montgomery_Distances = results
+saveRDS(Montgomery_Distances, "Miscellaneous/Montgomery_Distances.rds")
+Montgomery_Distances = readRDS("Miscellaneous/Montgomery_Distances.rds")
+
+
+
+meters_to_miles = function(x) {
+  return (x / 1609.344)
+}
+
+Montgomery_Distances = lapply(Montgomery_Distances,meters_to_miles)
+
+
+# results = gmapsdistance(origin = montgomeryCounty_origin,
+#                         destination = "38.989657+-92.310779",
+#                         mode = "driving")
