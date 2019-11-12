d0e90663ad10a7c97272ab19fb4384e777be90e9
enscully <ensxvd@mail.missouri.edu>
2019-11-12 12:53:07 -0600

    Started working on using the rds from the initial basis script instead of redoing the calculations

--- Miscellaneous/OptimalLocation.R
+++ Miscellaneous/OptimalLocation.R
@@ -9,61 +9,63 @@ Corn_FuturesMarket <- read_csv("Data/Corn_FuturesMarket.csv")
 Montgomery_Distances_DF = readRDS("Miscellaneous/Montgomery_Distances_DF.rds")
 
 # Load files
-cornBasis = read_csv("Miscellaneous/test.csv")
+cornBasis = read_csv("Miscellaneous/corn.csv")
 citiesCounties = read_excel("Miscellaneous/Cities and Counties.xlsx")
 
-# Clean text and get cities from K State data frame
-cornBasis$City = gsub(" MO", "", cornBasis$City, fixed = TRUE)
-cornBasis$City = trimws(cornBasis$City, "r")
-cornBasis$City = gsub(".", "", cornBasis$City, fixed = TRUE)
-cornBasis$City = tolower(cornBasis$City)
+kLocBasisMergeCorn = readRDS("Miscellaneous/KLocBasisMergeCorn.rds")
 
-# initialize column
-cornBasis$County = NA
-
-# Standardize data
-citiesCounties$CITY = tolower(citiesCounties$CITY)
-citiesCounties$COUNTY = tolower(citiesCounties$COUNTY)
-citiesCounties$CITY[which(citiesCounties$CITY == "e. prairie")] = "east prairie"
-
-# Matches counties to K State data
-for (i in seq_len(nrow(citiesCounties))) {
-  tempRows = which(cornBasis$City == citiesCounties$CITY[i])
-  cornBasis$County[tempRows] = as.character(citiesCounties$COUNTY[i])
-}
+# # Clean text and get cities from K State data frame
+# cornBasis$City = gsub(" MO", "", cornBasis$City, fixed = TRUE)
+# cornBasis$City = trimws(cornBasis$City, "r")
+# cornBasis$City = gsub(".", "", cornBasis$City, fixed = TRUE)
+# cornBasis$City = tolower(cornBasis$City)
+# 
+# # initialize column
+# cornBasis$County = NA
+# 
+# # Standardize data
+# citiesCounties$CITY = tolower(citiesCounties$CITY)
+# citiesCounties$COUNTY = tolower(citiesCounties$COUNTY)
+# citiesCounties$CITY[which(citiesCounties$CITY == "e. prairie")] = "east prairie"
+# 
+# # Matches counties to K State data
+# for (i in seq_len(nrow(citiesCounties))) {
+#   tempRows = which(cornBasis$City == citiesCounties$CITY[i])
+#   cornBasis$County[tempRows] = as.character(citiesCounties$COUNTY[i])
+# }
 
 # Clean and merge
 Montgomery_Distances_DF$County = tolower(Montgomery_Distances_DF$County)
-Montgomery_Merge = merge(x = cornBasis, y = Montgomery_Distances_DF, by = "County", all = TRUE)
-Corn_FuturesMarket$Date = mdy(Corn_FuturesMarket$Date)
+Montgomery_Merge = merge(x = kLocBasisMergeCorn, y = Montgomery_Distances_DF, by = "County", all = TRUE)
 
-# Makes list that separates each year into its own dataframe
-years = c("2014", "2015", "2016", "2017", "2018", "2019")
-commonCols = select(Montgomery_Merge, "County", "Week", "City", "Company", "Distance")
-Montgomery_List = list()
-for (Year in years) {
-  cols_For_Year = which(grepl(Year, colnames(Montgomery_Merge)))
-  Montgomery_List[[Year]] = data.frame(commonCols, 
-                                       Montgomery_Merge[, c(cols_For_Year)])
-  names(Montgomery_List[[Year]]) = c("County", "Week", "City", "Company", "Distance", "Date", "Basis")
-}
 
-# Gets the average basis for each county for each week
-get_County_Basis_Average = function(Data_One_Year) {
-  
-  Data_One_Year = Data_One_Year %>% 
-    select(-c("City", "Company")) %>%
-    group_by(County, Week, Date) %>%
-    summarise_each(list(mean = ~mean(., na.rm = TRUE))) %>%
-    mutate(Distance_mean = replace(Distance_mean, Distance_mean == "NaN", NA)) %>%
-    mutate(Basis_mean = replace(Basis_mean, Basis_mean == "NaN", NA))
-  
-  colnames(Data_One_Year) = c("County", "Week", "Date", "Distance", "avgBasis")
-  
-  return(Data_One_Year)
-}
+# # Makes list that separates each year into its own dataframe
+# years = c("2014", "2015", "2016", "2017", "2018", "2019")
+# commonCols = select(Montgomery_Merge, "County", "Week", "City", "Company", "Distance")
+# Montgomery_List = list()
+# for (Year in years) {
+#   cols_For_Year = which(grepl(Year, colnames(Montgomery_Merge)))
+#   Montgomery_List[[Year]] = data.frame(commonCols, 
+#                                        Montgomery_Merge[, c(cols_For_Year)])
+#   names(Montgomery_List[[Year]]) = c("County", "Week", "City", "Company", "Distance", "Date", "Basis")
+# }
 
-Montgomery_List = lapply(Montgomery_List, get_County_Basis_Average)
+# # Gets the average basis for each county for each week
+# get_County_Basis_Average = function(Data_One_Year) {
+#   
+#   Data_One_Year = Data_One_Year %>% 
+#     select(-c("City", "Company")) %>%
+#     group_by(County, Week, Date) %>%
+#     summarise_each(list(mean = ~mean(., na.rm = TRUE))) %>%
+#     mutate(Distance_mean = replace(Distance_mean, Distance_mean == "NaN", NA)) %>%
+#     mutate(Basis_mean = replace(Basis_mean, Basis_mean == "NaN", NA))
+#   
+#   colnames(Data_One_Year) = c("County", "Week", "Date", "Distance", "avgBasis")
+#   
+#   return(Data_One_Year)
+# }
+# 
+# Montgomery_List = lapply(Montgomery_List, get_County_Basis_Average)
 
 
 # Calculate differences between montgomery county and all other counties
@@ -131,31 +133,7 @@ split_tibble = function(tibble, col = 'col') tibble %>% split(., .[,col])
 weeklyAverageList = lapply(seq_len(length(weeklyAverageList)), function(x) split_tibble(weeklyAverageList[[x]], 'Week'))
 
 
-# 
-# tibble = weeklyAverageList[[1]]
-# col = "Week"
-# 
-# 
-# cleave_by <- function(df, ...) {
-#   stopifnot(inherits(df, "data.frame"))
-#   
-#   # use tidyeval to get the names ready for dplyr
-#   grouping <- quos(...)
-#   
-#   # Calculate a single number to represent each group
-#   group_index <- df %>%
-#     group_by(!!!grouping) %>%
-#     group_indices()
-#   
-#   # do the split by this single group_index variable and return it
-#   split(df, group_index)
-# }
-# 
-# function(tibble, col = 'col') {
-#   
-#   x = tibble %>% cleave_by(., .[,col])
-#   
-# }
+
 
 
 
@@ -198,51 +176,6 @@ ggplot(data = world) +
 # ggplot(data = stLouis) +
 #   geom_line(aes(x = Week, y = net, group = 1))
 
-
-
-
-
-# match_Futures_Prices = function(basis_List_Element) {
-#   initial_Merge = merge.data.frame(x = basis_List_Element, y = Corn_FuturesMarket, by = "Date", all.x = TRUE)
-#   
-#   # TODO Checking which dates need to be added which did not match
-#   if (length(which(!is.na(initial_Merge$Date) & is.na(initial_Merge$NearbyOC))) > 0) {
-#     dates_To_Find = unique(initial_Merge$Date[which(!is.na(initial_Merge$Date) & is.na(initial_Merge$NearbyOC))])
-#     
-#     replacement_Date = Corn_FuturesMarket$Date[which(abs(Corn_FuturesMarket$Date - dates_To_Find) == 
-#                                                        min(abs(Corn_FuturesMarket$Date - dates_To_Find)))[1]]
-#     
-#     rows_To_Replace = which(!is.na(initial_Merge$Date) & is.na(initial_Merge$NearbyOC))
-#     
-#     row_To_Copy = Corn_FuturesMarket[which(Corn_FuturesMarket$Date == replacement_Date), c("NearbyOC", "DecNC", "DecNC1yr", "DecNC2yr", "MarNC")]
-#     
-#     rows_Replaced = row_To_Copy %>% slice(rep(1:n(), each = length(rows_To_Replace)))
-#     
-#     initial_Merge[rows_To_Replace, c("NearbyOC", "DecNC", "DecNC1yr", "DecNC2yr", "MarNC")] = rows_Replaced
-#   }
-#   
-#   return(initial_Merge)
-# }
-# 
-# Montgomery_List = lapply(Montgomery_List, match_Futures_Prices)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 2.74 per loaded mile to truck grain one way
-
-# Montgomery_Merge$costToTruck = 
 
 
 
