c06141398cd574768fa20540b784d7678227f8e9
enscully <ensxvd@mail.missouri.edu>
2019-11-13 11:52:58 -0600

    Continued work on switching to 3 year average

--- Miscellaneous/OptimalLocation.R
+++ Miscellaneous/OptimalLocation.R
@@ -36,19 +36,21 @@ kLocBasisMergeCorn = readRDS("Miscellaneous/KLocBasisMergeCorn.rds")
 
 # Clean and merge
 Montgomery_Distances_DF$County = tolower(Montgomery_Distances_DF$County)
+Montgomery_Distances_DF = rbind(Montgomery_Distances_DF, c("montgomery", 0))
+Montgomery_Distances_DF$Distance = as.numeric(Montgomery_Distances_DF$Distance)
 Montgomery_Merge = merge(x = kLocBasisMergeCorn, y = Montgomery_Distances_DF, by = "County", all = TRUE)
 
 
-# # Makes list that separates each year into its own dataframe
-# years = c("2014", "2015", "2016", "2017", "2018", "2019")
-# commonCols = select(Montgomery_Merge, "County", "Week", "City", "Company", "Distance")
-# Montgomery_List = list()
-# for (Year in years) {
-#   cols_For_Year = which(grepl(Year, colnames(Montgomery_Merge)))
-#   Montgomery_List[[Year]] = data.frame(commonCols, 
-#                                        Montgomery_Merge[, c(cols_For_Year)])
-#   names(Montgomery_List[[Year]]) = c("County", "Week", "City", "Company", "Distance", "Date", "Basis")
-# }
+# Makes list that separates each year into its own dataframe
+years = c("threeYearAvg", "fiveYearAvg")
+commonCols = select(Montgomery_Merge, "County", "Distance", "geometry", "Latitude", "Longitude")
+Montgomery_List = list()
+for (Year in years) {
+  cols_For_Year = which(grepl(Year, colnames(Montgomery_Merge)))
+  Montgomery_List[[Year]] = data.frame(commonCols,
+                                       Montgomery_Merge[, c(cols_For_Year)])
+  names(Montgomery_List[[Year]]) = c("County", "Distance", "geometry", "Latitude", "Longitude", "avgBasis")
+}
 
 # # Gets the average basis for each county for each week
 # get_County_Basis_Average = function(Data_One_Year) {
@@ -74,12 +76,7 @@ Montgomery_Merge = merge(x = kLocBasisMergeCorn, y = Montgomery_Distances_DF, by
 get_Differences = function(Data_One_Year) {
   Data_One_Year$Difference = NA
   Montgomery_County = Data_One_Year[which(Data_One_Year$County == "montgomery"), ]
-  
-  for (i in 1:48) {
-    County_Rows = which(Data_One_Year$Week == i)
-    Week_Row = which(Montgomery_County$Week == i)
-    Data_One_Year$Difference[County_Rows] = Data_One_Year$avgBasis[County_Rows] - Montgomery_County$avgBasis[Week_Row]
-  }
+  Data_One_Year$Difference = Data_One_Year$avgBasis - Montgomery_County$avgBasis
   return(Data_One_Year)
 }
 
@@ -90,8 +87,13 @@ Number_Bushels = 10000
 Number_Truck_Loads = ceiling(Number_Bushels / 900)
 Cost_Per_Mile = 2.74
 
+
+# TODO Check if the rest of these functions work with the 3/5 year averages
+
+
 # Gets total cost of trucking per bushel
 get_Trucking_Total = function(Data_One_Year) {
+  Data_One_Year$Distance = as.numeric(Data_One_Year$Distance)
   Data_One_Year$truckingTotal = NA
   Data_One_Year$truckingTotal = -((Data_One_Year$Distance * Cost_Per_Mile * Number_Truck_Loads) / Number_Bushels)
   return(Data_One_Year)
