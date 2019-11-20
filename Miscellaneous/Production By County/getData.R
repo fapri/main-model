aa8c9d1d00415eb40674dc5301af881db8c87119
enscully <ensxvd@mail.missouri.edu>
2019-11-20 12:49:10 -0600

    Pulled data from NASS API

---
+++ Miscellaneous/Production By County/getData.R
@@ -0,0 +1,19 @@
+library(httr)
+library(jsonlite)
+
+url = "http://quickstats.nass.usda.gov/api/api_GET/?key="
+key = "7BCDD118-BF8E-3640-951C-9A2748047903"
+commodity = "&commodity_desc=CORN"
+stateName = "&state_name=MISSOURI"
+statistcCat = "&statisticcat_desc=PRODUCTION"
+year = "&year__GE=2014"
+
+call = paste(url, key, commodity, stateName, statistcCat, year, sep = "")
+
+get_Data = GET(call)
+
+to_Text = content(get_Data, "text")
+
+from_JSON = fromJSON(to_Text, flatten = TRUE)
+
+productionFinal = as.data.frame(from_JSON)
